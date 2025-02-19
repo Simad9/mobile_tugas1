29e5bada4ce25b65751b94a2176474072c3ed0ed
vrida pusparani <vridapusparani@gmail.com>
2025-02-20 05:15:41 +0700

    menambahkan error handling

--- lib/pages/menu/hitung_page.dart
+++ lib/pages/menu/hitung_page.dart
@@ -13,18 +13,29 @@ class _HitungPageState extends State<HitungPage> {
   final TextEditingController num2Controller = TextEditingController();
   String result = "";
   String operator = "+";
-
   void calculate(String operation) {
-    // Control Handling
+    // Validasi input
     if (num1Controller.text.isEmpty || num2Controller.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Masukkan kedua angka terlebih dahulu!')),
+      );
       return;
     }
-
-    // Fungsi
-    double num1 = double.tryParse(num1Controller.text) ?? 0;
-    double num2 = double.tryParse(num2Controller.text) ?? 0;
+    if (num1Controller.text.length > 16 || num2Controller.text.length > 16) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Input tidak boleh lebih dari 16 digit!')),
+      );
+      return;
+    }
+    double? num1 = double.tryParse(num1Controller.text);
+    double? num2 = double.tryParse(num2Controller.text);
+    if (num1 == null || num2 == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Input harus berupa angka!')),
+      );
+      return;
+    }
     double res = 0;
-
     switch (operation) {
       case '+':
         res = num1 + num2;
@@ -39,205 +50,206 @@ class _HitungPageState extends State<HitungPage> {
         res = num2 != 0 ? num1 / num2 : 0;
         break;
     }
-
+    res = double.parse(res.toStringAsFixed(4));
     setState(() {
       result = res.toString();
     });
   }
-
-  void uiHitung(String opration) {
+  void uiHitung(String operation) {
     setState(() {
-      operator = opration;
+      operator = operation;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // button mbalik
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => HomePage()));
-            },
-            child: Image.asset(
-              "images/backbutton.png",
-              width: 32,
-              height: 32,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // button mbalik
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => HomePage()));
+              },
+              child: Image.asset(
+                "images/backbutton.png",
+                width: 32,
+                height: 32,
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          // judul
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              "Hitung",
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            SizedBox(height: 20),
+            // judul
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Hitung",
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            "Silahkan masukan angka dibawah",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-          ),
-          SizedBox(height: 20),
-          // Input Username
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: num1Controller,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: "Angka",
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
+            SizedBox(height: 20),
+            Text(
+              "Silahkan masukan angka dibawah",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 20),
+            // Input Username
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: num1Controller,
+                    keyboardType: TextInputType.numberWithOptions(decimal: true),
+                    maxLength: 16, // Batasan maksimal 16 digit
+                    decoration: InputDecoration(
+                      hintText: "Angka",
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              ElevatedButton(
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    operator == "" ? '+' : operator,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    controller: num2Controller,
+                    keyboardType: TextInputType.numberWithOptions(decimal: true),
+                    maxLength: 16,
+                    decoration: InputDecoration(
+                      hintText: "Angka",
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var op in ['+', '-', '*', '/'])
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () => uiHitung(op),
+                      child: Text(
+                        op,
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            SizedBox(height: 10),
+            // tombol kalkulasi
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
                 ),
-                onPressed: () {},
-                child: Text(
-                  operator == "" ? '+' : operator,
+                onPressed: () => calculate(operator),
+                child: const Text(
+                  "Hitung Pak",
                   style: TextStyle(
-                      fontSize: 20,
-             
                       color: Colors.white,
+                      fontSize: 20,
                       fontWeight: FontWeight.w500),
                 ),
               ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: TextField(
-                  controller: num2Controller,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: "Angka",
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
+                  colors: [
+                    Color(0xff4DC5F2),
+                    Color(0xffFF61E4),
+                  ],
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              for (var op in ['+', '-', '*', '/'])
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+              child: Column(
+                children: [
+                  Text(
+                    "Hasil yang didapat",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    child: Center(
+                      child: Text(
+                        result == "" ? "Angka" : result,
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black54,
+                            fontWeight: FontWeight.w600),
                       ),
                     ),
-                    onPressed: () => uiHitung(op),
-                    child: Text(
-                      op,
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500),
-                    ),
                   ),
-                ),
-            ],
-          ),
-          SizedBox(height: 10),
-          // tombol kalkulasi
-          SizedBox(
-            width: double.infinity,
-            height: 50,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-              onPressed: () => calculate(operator),
-              child: const Text(
-                "Hitung Pak",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500),
-              ),
-            ),
-          ),
-          SizedBox(height: 30),
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-                begin: Alignment.centerRight,
-                end: Alignment.centerLeft,
-                colors: [
-                  Color(0xff4DC5F2),
-                  Color(0xffFF61E4),
                 ],
               ),
-            ),
-            child: Column(
-              children: [
-                Text(
-                  "Hasil yang didapat",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                  ),
-                  child: Center(
-                    child: Text(
-                      result == "" ? "Angka" : result,
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black54,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
