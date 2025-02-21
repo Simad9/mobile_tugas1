d65e9bc8b06b996dffb965007355dae1877cbbca
aqshajr <139189331+aqshajr@users.noreply.github.com>
2025-02-22 00:45:21 +0700

    Add files via upload
    solve overflow

---
+++ login_page.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:tugas1/import/all_pages.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerRight,
+            end: Alignment.centerLeft,
+            colors: [
+              Color(0xff4DC5F2),
+              Color(0xffFF61E4),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding:
+                EdgeInsets.only(left: 30, right: 30, top: 120, bottom: 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Halaman Masuk",
+                      style: TextStyle(
+                          fontSize: 36,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Pagi pagi pergi ke pasar membeli lele. Sebelum masuk loginkan dulu le.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                  left: 20, right: 20, top: 40, bottom: 190 - 23),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Input Username
+                  TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+
+                  // Lupa Password
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text("Lupa Password Pak"),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+
+                  // Tombol Masuk
+                  _buttonHitam(context),
+                  const SizedBox(height: 15),
+
+                  // Info tambahan
+                  const Center(
+                    child: Text(
+                      "Silahkan hubungi admin untuk mendaftarkan diri\nuntuk bisa masuk ke aplikasi.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonHitam(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        onPressed: () {
+          if (usernameController.text == "admin" &&
+              passwordController.text == "admin") {
+            // Implement login logic here
+            FocusScope.of(context).unfocus();
+
+            Future.delayed(Duration(milliseconds: 300), ()
+            {
+              Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            });
+          } else {
+            // Tampilkan pesan error jika username atau password salah
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Username atau password salah")),
+            );
+          }
+        },
+        child: const Text(
+          "Masuk",
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+}
